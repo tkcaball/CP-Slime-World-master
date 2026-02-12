4a16ffa5a68495ce3a88b1625ec613ffc2379d75
ticaballero2 <ticaballero2@gmail.com>
2026-02-11 22:11:35 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Code Practice: Slime World
+// Name: Tiffany Caballero
+// Date: 2/11/26
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    width: 320,
+    height: 240,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    zoom: 2,
+    scene: [ Overworld ]
+}
+
+const game = new Phaser.Game(config)
